f36b18e2f578547e2b8d87fca89b5bbe75833383
Morten J <morten2094@hotmail.com>
2016-08-27 13:57:31 +0200

    main menu

---
+++ mammoth_mayhem/Assets/scirpts/MainMenuHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void loadGameScene(){
+		SceneManager.LoadScene ("GameScene");
+	}
+
+	public void quitGame(){
+		Application.Quit ();
+	}
+}
